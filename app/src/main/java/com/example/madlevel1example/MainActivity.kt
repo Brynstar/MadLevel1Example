8ccdfbbf9993af27ff124341ead589d50efcd8ee
Bryn van Dam <Bryn.vanDam@nice.com>
2020-09-14 01:01:14 +0200

    removed comments

--- app/src/main/java/com/example/madlevel1example/MainActivity.kt
+++ app/src/main/java/com/example/madlevel1example/MainActivity.kt
@@ -11,10 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root) // Sets the activity layout resource file.
+        setContentView(binding.root)
 
-        // Using the id given in the layout file you can access the component.
-        // Set an action when the user clicks on the confirm button.
         binding.btnConfirm.setOnClickListener {
             checkAnswer()
         }
@@ -27,8 +25,6 @@ class MainActivity : AppCompatActivity() {
     private fun checkAnswer() {
         val answer = binding.etAnswer.text.toString()
 
-        // When the answer equals "giraffe" then display a correct message using a toast message.
-        // Otherwise display an incorrect message.
         if (answer == getString(R.string.giraffe)) {
             Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
         } else {
